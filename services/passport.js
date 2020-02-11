6c1294eb3429d97cb266f4f453c66a8842fcf757
Sophie Jung <sophiecheong7@gmail.com>
2020-02-11 14:35:52 +0800

    Added passport.serializeUser/deserializeUser

--- services/passport.js
+++ services/passport.js
@@ -35,3 +35,13 @@ passport.use(
         }
     )
 );
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
