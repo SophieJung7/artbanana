e8e53b41f9d70c084175904c47d4f5a6c1c4075f
Sophie Jung <sophiecheong7@gmail.com>
2020-02-11 10:49:53 +0800

    Mongoose userSchema setup

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+//Mongoose --> Don't change the order. userSchema should be called before passport.js.
+require('./models/User');
+mongoose.connect(keys.mongoURI);
 
 //Passport 
 require('./services/passport');
 
 //Routes
 require('./routes/authRoutes')(app);
+
 
 
 const PORT = process.env.PORT || 5000;
