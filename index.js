0e1dc0652bdd76f6aa66369b56d99da9bf2ecea1
Sophie Jung <sophiecheong7@gmail.com>
2020-02-12 14:21:53 +0800

    Google OAuth Setup Finished

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 
-//Mongoose --> Don't change the order. userSchema should be called before passport.js.
+//Connecting Mongoose --> Don't change the order. userSchema should be called before passport.js.
 require('./models/User');
 mongoose.connect(keys.mongoURI);
 
-//Passport 
+//Connecting Passport 
 require('./services/passport');
+
+//Enabling Cookies 
+app.use(cookieSession({
+    //Cookie lasts for 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [ keys.cookieKey ]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Routes
 require('./routes/authRoutes')(app);
