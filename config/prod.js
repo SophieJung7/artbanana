37e64098d42c0c1dc5b976d2d6ace498f84ac68d
Sophie Jung <sophiecheong7@gmail.com>
2020-02-13 11:21:11 +0800

    Added naverClientID/Secret at for production mode

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    naverClientID: process.env.NAVER_CLIENT_ID,
+    naverClientSecret: process.env.NAVER_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 };
