9dc5478701f32ef4c04fb88063e7c86c856e742f
Sophie Jung <sophiecheong7@gmail.com>
2020-03-06 11:20:50 +0800

    Added lowercase test to pasword Regex

--- client/src/components/utils/validatePassword.js
+++ client/src/components/utils/validatePassword.js
@@ -1,8 +1,13 @@
 export default password => {
   const minMaxLength = /^[\s\S]{6,100}$/;
   const number = /[0-9]/;
-  if (minMaxLength.test(password) && number.test(password)) {
+  const lower = /[a-z]/;
+  if (
+    minMaxLength.test(password) &&
+    number.test(password) &&
+    lower.test(password)
+  ) {
     return;
   }
-  return `비밀번호는 6자 이상 입력해주시고 숫자를 1개 이상 사용해주세요.`;
+  return `비밀번호는 6자 이상 입력해주시고 숫자 및 영문 소문자를 1개 이상 사용해주세요.`;
 };
