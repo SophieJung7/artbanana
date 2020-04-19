133f6b0bf1328a9da7f6e3062046b7497de1ef64
Sophie Jung <sophiecheong7@gmail.com>
2020-04-19 21:14:34 +0800

    Finish deploy configurations

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
+
+// mongoURI: 'mongodb://127.0.0.1:27017/interviewon_ci'
--- index.js
+++ index.js
@@ -49,16 +49,16 @@ app.use(
 require('./routes/authRoutes')(app);
 require('./routes/proRoutes')(app);
 
-//NODE_ENV comes from Heroku setup
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets like our main.js file, or main.css!
+if (['production', 'ci'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
-  // Express will serve up the index.html file if it doesn't recognize the route!
+
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
