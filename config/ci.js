881cba897f34b8cd8420e60bc33467cd8ce69ff2
Sophie Jung <sophiecheong7@gmail.com>
2020-04-20 10:39:10 +0800

    Add more configurations like naverClientID to ci.js

--- config/ci.js
+++ config/ci.js
@@ -1,8 +1,12 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  naverClientID: process.env.NAVER_CLIENT_ID,
+  naverClientSecret: process.env.NAVER_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  sendgridAPI: process.env.SENDGRID_API,
+  redirectDomain: 'http://localhost:3000',
 };
 
 // mongoURI: 'mongodb://127.0.0.1:27017/interviewon_ci'
