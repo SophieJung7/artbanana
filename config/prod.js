4a9a0ae8f931f3342106ea86c136572601192933
Sophie Jung <sophiecheong7@gmail.com>
2020-04-20 10:53:42 +0800

    Change redirectDomain for prod.js

--- config/prod.js
+++ config/prod.js
@@ -6,5 +6,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   sendgridAPI: process.env.SENDGRID_API,
-  redirectDomain: 'https://cat-snap.herokuapp.com'
+  redirectDomain: 'https://interviewon.herokuapp.com',
 };
